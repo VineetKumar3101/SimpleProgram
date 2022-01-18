a19731eccd84fd9245fc9d131a116a6b89136b8f
Vineet Kumar <vineet.kumar_cs20@gla.ac.in>
2022-01-18 11:20:49 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+print("Hello World")
+
+#sum
+num1=80
+num2=90
+sum=num1+num2
+print(sum)
+
+num1=input("Enter")
+num2=input("Enter")
+sum=num1+num2  #it concatinates the string
+print(sum)
+
+#int value
+num1=int(input("Enter"))     #it adds the string
+num2=int(input("Enter"))
+sum=num1+num2
+print(sum)
+
+#float value
+num1=float(input("Enter"))
+num2=float(input("Enter"))
+sum=num1+num2
+print(sum)
+
+#Eval  For any data type
+num1=eval(input("Enter"))
+num2=eval(input("Enter"))
+sum=num1+num2
+print(sum)
+
+
+#Simple Interest
+P,R,T=1000,10,5
+Si=(P*R*T)/100
+print(Si)
+
+#Simple Interest with user
+P=float(input("Enter"))
+R=float(input("Enter"))
+T=float(input("Enter"))
+Si=(P*R*T)/100
+print(Si)
